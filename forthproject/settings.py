3b32f6d58ecdb96b8423625ec9f5e8d53c350577
minakusi <seoyeon737@gmail.com>
2019-07-30 15:49:45 +0900

    final commit

--- forthproject/settings.py
+++ forthproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!&7@0i7ww9ldi4_beb3jx$zdsd=daue4%@180uvouu92-ndhz'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'b!&7@0i7ww9ldi4_beb3jx$zdsd=daue4%@180uvouu92-ndhz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- myblog/views.py
+++ myblog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
 from django.utils import timezone
+from django.core.paginator import Paginator
+from .form import BlogPost
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects
-    return render(request, 'home.html',{'blogs':blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'home.html',{'blogs':blogs, 'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
@@ -21,3 +27,15 @@ def create(request):
     blog.body = request.GET['body']
     blog.save()
     return redirect('/blog/'+str(blog.id))
+
+def blogpost(request):
+    if request.method == 'POST':
+        form = BlogPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect('home')    
+    else:
+        form = BlogPost()
+        return render(request, 'new.html', {'form':form})
